5c371331a9a70279e474bcd41e0bc7116f0105fc
Rendy Artha <rendyartha312@gmail.com>
2021-03-16 11:25:32 +0700

    setup user per page

--- src/App.js
+++ src/App.js
@@ -4,11 +4,24 @@ import ReactPaginate from 'react-paginate';
 import React, { useState } from 'react';
 
 function App() {
-  const [state, setState] = useState(JsonData.slice(0, 50));
-  console.log(state)
+  const [users, setUsers] = useState(JsonData.slice(0, 50));
+  const [pageNumber, setPageNumber] = useState(0)
+
+  const userPerPage = 10
+  const pagePerVisited = pageNumber * userPerPage
+  const displayUsers = users
+    .slice(pagePerVisited, pagePerVisited + userPerPage)
+    .map(user => (
+      <div key={user.id}>
+        <span>{user.first_name}</span>
+        <span> {user.last_name}</span>
+      </div>
+    ))
+
   return (
     <div className="App">
-      
+      <h1>List Users</h1>
+      {displayUsers}
     </div>
   );
 }
