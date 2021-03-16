ce666daf66edb63d2079a2b3a9cf68891d3a1b1f
Rendy Artha <rendyartha312@gmail.com>
2021-03-16 10:36:00 +0700

    setup data pagination and react-paginate

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
+import JsonData from './MOCK_DATA.json';
+import ReactPaginate from 'react-paginate';
+import React, { useState } from 'react';
 
 function App() {
+  const [state, setState] = useState(JsonData.slice(0, 50));
+  console.log(state)
   return (
     <div className="App">
       
